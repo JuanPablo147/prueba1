b1875903ea7cfb23588681fd07d0e4a9a1172c83
JuanPablo147 <93529743+JuanPablo147@users.noreply.github.com>
2022-04-26 12:23:29 -0400

    Update main.java
    Nuevo Metodo

--- main.java
+++ main.java
@@ -4,4 +4,8 @@ public class main{
     public static void main(String[]args){
         System.out.println("hola mundo");
     }
+    public void pedro(){
+        System.out.println("hola mundo");
+    }
+    
 }
